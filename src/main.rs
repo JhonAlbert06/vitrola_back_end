d97f64586aecbbb6e026d0d38d6c9c53ef0bf75c
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2023-02-22 14:10:34 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
 #[macro_use] extern crate rocket;
 
-#[get("/")]
-fn hello() -> &'static str {
-    "Hello, world!"
+#[get("/Songs")]
+fn get_songs() -> &'static str {
+    "Get Songs!"
+}
+
+#[post("/Songs")]
+fn post_songs() -> &'static str {
+    "Post Songs!"
+}
+
+#[put("/Songs")]
+fn update_songs() -> &'static str {
+    "Update Songs!"
+}
+
+#[delete("/Songs")]
+fn delete_songs() -> &'static str {
+    "Delete Songs!"
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello])
+    rocket::build().mount("/", routes![get_songs, post_songs, update_songs, delete_songs])
 }
